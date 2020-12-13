7c7cd988d612a6ba69321dd02f32338e36a76667
omosteven <omosteven123@gmail.com>
2020-12-13 01:37:24 +0100

    Done

--- src/containers/home/components/HomeBody.js
+++ src/containers/home/components/HomeBody.js
@@ -9,6 +9,7 @@ import {Helmet} from "react-helmet";
 import {Chrono} from 'react-chrono';
 
 // import {Ripple} from "react-preloaders";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import mobileFirstImage from "../images/Startup_SVG-a7c5e6d7.svg";
 
@@ -17,50 +18,41 @@ import mobileSecondImage from "../images/intellectual-b138e111.svg";
 const HomeBody = () => {
     const data = [
         {
-            title: 'Step 0 - As a Customer or Business.',
+            title: 'Step 0 - As a Customer or Service provider.',
             contentText: 'DOWNLOAD APP.',
-            contentDetailedText: 'Get the APP installed from PlayStore or AppStore.'
+            contentDetailedText: 'Get the APP installed from PlayStore or AppStore and set up your account.'
         },
         {
-            title: 'Step 1 - As a Business',
+            title: 'Step 1 - As a Service provider.',
 
-            contentText: 'SET UP ACCOUNT',
-            contentDetailedText: 'Create an account and set up your profile'
+            contentText: 'CHECK YOUR TIMELINE.',
+            contentDetailedText: 'Check your timeline to see customers around you that requested for a specific service. You will always receive notifications for new requests.'
         },
         {
-            title: 'Step 1 - As a Customer.',
-
-            contentText: 'SET UP ACCOUNT.',
-            contentDetailedText: 'Create an account and set up your profile.'
-        },
-        {
-            title: 'Step 3',
-            contentText: 'SET UP PROFILE',
-            contentDetailedText: 'Create a personal profile and add neccessary info about you'
-        }, {
             title: 'Step 2 - As a Customer.',
-            contentText: 'SEARCH FOR SERVICE.',
+            contentText: 'SEARCH FOR A SERVICE.',
             contentDetailedText: 'Search for any service and read about the description of which is of your interest.'
-        }, {
-            title: 'Step 2',
-            contentText: 'SEARCH FOR SERVICE',
-            contentDetailedText: ''
+        },
+        {
+            title: 'Step 2 - As a Service provider.',
+            contentText: 'CHAT CUSTOMERS.',
+            contentDetailedText: 'Chat up customers that request for your service.'
         }, {
             title: 'Step 3 - As a Customer.',
-            contentText: 'MAKE REQUEST.',
+            contentText: 'MAKE A REQUEST.',
             contentDetailedText: 'Make a request with just a click and get redirected to chat up the service provider.'
         }, {
-            title: 'Step 2',
-            contentText: 'SEARCH FOR SERVICE',
-            contentDetailedText: ''
+            title: 'Step 3 - As a Service provider.',
+            contentText: 'DELIVER & GET PAID.',
+            contentDetailedText: 'Deliver your service to your customer as agreed.'
         }, {
             title: 'Step 4 - As a Customer.',
             contentText: 'PAY & WAIT FOR DELIVERY.',
             contentDetailedText: 'Make your payment and rate service after delivery.'
         }, {
-            title: 'Step 4 - As a Business.',
-            contentText: 'PAY & WAIT FOR DELIVERY.',
-            contentDetailedText: 'Make your payment and rate service after delivery.'
+            title: 'Step 4 - As a Service provider.',
+            contentText: 'POST UPDATES/NEWS.',
+            contentDetailedText: 'Post your News & recent updates to your customerss (those that favorite you) Timeline.'
         }, {
             title: 'Step 4 - As a Customer.',
             contentText: 'ADD SERVICE AS FAVORITE.',
@@ -88,16 +80,15 @@ const HomeBody = () => {
                         <div className="first__layer">
                             <div className="left__side">
                                 <div className="left__side__container">
-                                    <p className="perfect__place animate--perfect">
-                                        Welcome to
-                                        <span class="ServiceandMe">ServiceandMe</span>
-                                    </p>
-                                    <p className="perfect__place animate--perfect">
-                                        {/* ServiceandMe */} </p>
+                                    <h2 className="perfect__place animate--perfect">
+                                        Welcome to {/* <span class="ServiceandMe">ServiceandMe</span> */} </h2>
+                                    <h2 className="perfect__place animate--perfect">
+                                        ServiceandMe
+                                    </h2>
                                     <h4 className="service__describe_page">
                                         An online platform that allows you as a service
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        provider to render your services (on-demand) to people around
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        you OR connect with Nearby service providers as customer.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    provider to render your services (on-demand) to people around
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    you OR connect with Nearby service providers as customer.
                                     </h4>
 
                                     <div className="left__side__button__container">
@@ -147,20 +138,69 @@ const HomeBody = () => {
                                     className="second__layer__desc__img"/>
                                 <div className="second__layer__desc">
                                     <h5 className="second__layer__desc__head">
-                                        Change your business with ServiceandMe
+                                        RENDER and EARN
                                     </h5>
 
                                     <li className="second__layer__desc__inner">
-                                        ServiceandMe is a platform that connects customers with on-demand
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        businesses around them. On demand businesses are businesses
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        which deliver their product at your door from anywhere and
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        anytime
+                                        ServiceandMe is a platform that connects customers with on-demand businesses around them. On demand businesses are businesses which deliver their product at your door from anywhere and
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    anytime
                                     </li>
                                     <li className="second__layer__desc__inner">
-                                        ServiceandMe allows users to render their services as service
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        provider to people around them and connect with those services
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        providers as customers.
+                                        We allow you to turn your mobile phone to your business office where you can earn by REDERING service OR connect you with service providers.
                                     </li>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    {/*  */}
+                    <section className="__layer" data-aos="zoom-in" data-aos-offset="100" data-aos-easing="ease-in-sine">
+                        {/* <hr className="layer__rule" /> */}
+                        <div className="__layer__container cardContWidth">
+                            <h4 className="second__layer__title">Why ServiceandMe?</h4>
+                            <h5 className="why__subititle">Are you a Customer or Service provider?</h5>
+                            <div className="container">
+                                <div className="row mt-3">
+                                    <div className="col-sm-12 col-lg-6 col-xl-6 col-md-6 cardCont">
+                                        <h6 className="whyCardTitle">As a Service provider</h6>
+                                        <div className="why--card">
+                                            <h6 className="whyItem">
+                                                <i class="fa fa-money whyIcon" aria-hidden="true"></i>
+                                                Make Money When Delivering Services.</h6>
+                                        </div>
+                                        <div className="why--card">
+                                            <h6 className="whyItem">
+                                                <i class="fa fa-save whyIcon" aria-hidden="true"></i>
+                                                Save Money From Developing A Dedicated Website For Your Business.</h6>
+                                        </div>
+                                        <div className="why--card">
+                                            <h6 className="whyItem">
+                                                <i class="fa fa-expand whyIcon" aria-hidden="true"></i>
+                                                Gain more customers.</h6>
+                                        </div>
+                                        <div className="why--card">
+                                            <h6 className="whyItem">
+                                                <i class="fa fa-credit-card whyIcon" aria-hidden="true"></i>
+                                                Accept payment directly from your customer on the app.</h6>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-12 col-lg-6 col-xl-6 col-md-6 cardCont">
+                                        <h6 className="whyCardTitle">As a Customer.</h6>
+                                        <div className="why--card">
+                                            <h6 className="whyItem">
+                                                <i class="fa fa-save whyIcon" aria-hidden="true"></i>
+                                                Save stress; you can locate your business with a click.</h6>
+                                        </div>
+                                        <div className="why--card">
+                                            <h6 className="whyItem">
+                                                <i class="fa fa-link whyIcon" aria-hidden="true"></i>
+                                                Stay connected; you can receive News/trends/promo from your favorite service providers.</h6>
+                                        </div>
+                                        <div className="why--card">
+                                            <h6 className="whyItem">
+                                                <i class="fa fa-credit-card whyIcon" aria-hidden="true"></i>
+                                                Pay on the go; you can pay for service delivered on the app using your bank card.</h6>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -224,8 +264,7 @@ const HomeBody = () => {
             {/* <Ripple color={"rgb(0, 68, 102)"}
                 background="white"
                 time={3000}
-                customLoading={false}/> */}
-        </>
+                customLoading={false}/> */} </>
     );
 };
 
